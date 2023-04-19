43116063830918c0d4c5be1f712308d7cb4ad519
Salwyn <salwynmathew13@gmail.com>
2023-04-20 03:46:45 +0530

    new commit

--- Generator_engine.py
+++ Generator_engine.py
@@ -10,3 +10,14 @@ def generate_response(prompt):
     ])
     response = completion["choices"][0]["message"]['content']
     return response
+
+# completion = openai.ChatCompletion.create(
+#   model="gpt-3.5-turbo",
+#   messages=[
+#         {"role": "system", "content": "You are a professional Insurance Agent in India"},
+#         {"role": "user", "content": "How much compensation can I get if my Car is dented from the bonet?"},
+#     ] 
+# )
+
+# reply_content = completion["choices"][0]["message"]
+# print(reply_content)
