8c61f508ee57911aeb6e62e55130f869a1ca28e1
Salwyn <salwynmathew13@gmail.com>
2023-05-01 19:53:52 +0530

    react app

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI
 from Generator_engine import generate_response
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins="http://127.0.0.1:3000/",
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
 def root():
-    return {"Hello ": "World"}
+    return {"msg": "Hello Goodbye"}
 
 @app.post("/generate_response")
 def generate_chat(prompt: str):
